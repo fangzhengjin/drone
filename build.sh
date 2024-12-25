ff6a2883e8d73bbb772f3455c87b7b12930b6c1d
ZhengJin <fangzhengjin@gmail.com>
2024-12-25 15:08:41 +0800

    1

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker run --rm -e GO111MODULE=on -e GOPROXY=https://goproxy.cn -v $PWD:/workspace 172.17.27.51/docker.io/golang:1.14.15 sh -c "cd /workspace && GOARCH=amd64 GOOS=linux scripts/build.sh"
+docker run --rm -e GO111MODULE=on -e GOPROXY=https://goproxy.cn -v $PWD:/workspace 172.17.27.51/docker.io/golang:1.22.7 sh -c "cd /workspace && GOARCH=amd64 GOOS=linux scripts/build.sh"
 DOCKER_IMAGE=172.17.27.51/drone/drone:$(date '+%Y%m%d%H%M%S')
 docker build -t $DOCKER_IMAGE -f docker/Dockerfile.server.linux.amd64 .
 docker push $DOCKER_IMAGE
